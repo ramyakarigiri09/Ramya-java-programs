d6b88526d8839bc6fd90f1a0b66b4b07b9d4cb59
ramyakarigiri09 <ramyakarigiri09@gmail.com>
2026-01-13 13:49:37 -0600

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str= "This is Ramya- java programs";
+        String reverseStr = "";
+         Char ch;
+
+        for (int i = 0; i<str.length();  i++) {
+         ch = str.charAt(i);
+            reverseStr = ch + reverseStr;
+        }
+
+        System.out.println("Reversed String is : " + reversed);
+    }
+}
